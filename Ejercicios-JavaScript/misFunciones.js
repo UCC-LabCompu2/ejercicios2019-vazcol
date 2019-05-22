ca833b4b720b3265bedb990063ffb874650f6711
vazcol <matias.vazquezdelacolina@gmail.com>
2019-05-22 09:48:05 -0300

    Se empezo a resolver ejercicios de javascritp

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,24 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function saludar() {
+    alert("Hola Mundo");
+}
+
+function converter(id_elem,valor_elem) {
+    if (isNaN(valor_elem)){
+        alert("El valor de "+id_elem+" debe ser numerico.");
+        document.getElementById(id_elem).value = "";
+    }
+
+    if(!isNaN(valor_elem)){
+        if(id_elem=='metro'){
+        document.getElementById("pulgada").value = valor_elem*39.3701
+        }
+        if(id_elem=='pulgada'){
+            
+        }
+
+    }
+
+}
